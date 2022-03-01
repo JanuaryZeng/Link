576a8c3af46cb606a370f750c244bdc763b7207e
JanuaryZeng <15841331413@163.com>
2022-03-01 14:54:03 +0800

    7.6.1

--- CSAPP/7_6/0/main.cpp
+++ CSAPP/7_6/0/main.cpp
@@ -4,6 +4,28 @@ void swap(int i1, int i2){
 void swap(int i1, int i2, int i3){
     i3 = i1;
 }
+class C{
+int cc;
+};
+
+class D{
+int *dd;
+};
+
+class E{
+public:
+E(int val):ee(val), eeptr(&ee){
+}
+int ee;
+int *eeptr;
+};
+
+int *a;
+int b;
+C c;
+D d;
+//E e(1);
+//int f = 0;
 
 int main(){
     swap(1,2,3);
